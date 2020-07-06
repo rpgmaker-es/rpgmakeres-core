522b8712d4bbafade0fa75cd34fb665b0d4708cb
Francisco de la Peña <fran@fran.cr>
2020-07-05 19:32:15 -0600

    Properly toggle nav items after new defaults

--- public/script/header_userbuttons.js
+++ public/script/header_userbuttons.js
@@ -1,6 +1,9 @@
 if (sessionStorage.getItem("rpgmakeres_sesion") !== null) {
     document.getElementById("menu_user_acceso").style.display = "none";
+    document.getElementById("menu_user_ajustes").style.display = "block";
+    document.getElementById("menu_user_salir").style.display = "block";
 } else {
+    document.getElementById("menu_user_acceso").style.display = "block";
     document.getElementById("menu_user_ajustes").style.display = "none";
     document.getElementById("menu_user_salir").style.display = "none";
 }
