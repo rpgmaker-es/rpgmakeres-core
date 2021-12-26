41224c04eb724b2cced48aaf87a126575a76ee49
Francisco de la Peña <fran@fran.cr>
2021-12-25 19:59:39 -0600

    Test body with special characters

--- views/testMail.php
+++ views/testMail.php
@@ -1,3 +1,3 @@
-<h1>RPG Maker ES Core</h1>
+<h1>RPG Maker ES Core (RPG Maker en Español)</h1>
 <p>This is a mail generated from RPG Maker ES Core, rendered from a view, sent to <?=$mail?></p>
 <p>For functionality demonstrating, here's a random number: <strong><?=$randomic?></strong></p>
